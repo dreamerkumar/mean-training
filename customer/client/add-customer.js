5b04e3cdceeba199b9a43ff2c5987e99f5adae21
Vishal-Kumar <kumarfromatlanta@hotmail.com>
2016-12-04 22:55:17 -0500

    hook up add service to backend

--- customer/client/add-customer.js
+++ customer/client/add-customer.js
@@ -1,7 +1,19 @@
 angular.module('mtCustomer')
     .component('addCustomer', {
         templateUrl: '/customer-client/add-customer.html',
-        controller: function() {
+        controller: function($http, $state) {
             var ctrl = this;
+
+            ctrl.addCustomer = function() {
+                $http.post('/customer', {
+                    firstName: ctrl.firstName,
+                    lastName: ctrl.lastName,
+                }).then(function(status){
+                    console.log(status);
+                    $state.go('customerList');
+                }).catch(function(error){
+                    console.error(error);
+                });
+            }
         }
     });
