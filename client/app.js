b6d0ba4a4860827e36114013e8b1b6be45d0a542
Vishal-Kumar <kumarfromatlanta@hotmail.com>
2016-12-04 22:29:15 -0500

    Adding routes for customer list and adding a customer

--- client/app.js
+++ client/app.js
@@ -1,1 +1,13 @@
-angular.module('mtApp', ['mtCustomer']);
+angular.module('mtApp', ['ui.router', 'mtCustomer'])
+    .config(function($stateProvider){
+        $stateProvider
+            .state('customerList',{
+                url: '/customer-list',
+                template: '<customer-list></customer-list>'
+            })
+            .state('addCustomer',{
+                url: '/add-customer',
+                template: 'Add customer markup goes here'
+            });
+
+    });
